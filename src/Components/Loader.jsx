ffa7c048dc6e24a3afa6f887d4f9a033a64c0f54
Nikhil Waghade <nikhilwaghade031003@gmail.com>
2025-05-17 19:42:25 +0530

    update loader

--- src/Components/Loader.jsx
+++ src/Components/Loader.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import logo from '../assets/logo.jpg';
 
 const Loader = () => {
   return (
     <div className="loader-overlay flex justify-center items-center fixed top-0 left-0 w-full h-full bg-white dark:bg-gray-900 z-50">
       <img
-        src="src\assets\logo.jpg" // Add the path to your logo here
+        src={logo} 
         alt="Loading..."
-        className="animate-spin h-20 w-20" // You can add a spin animation or modify this to fit your style
+        className="animate-spin h-20 w-20" 
       />
     </div>
   );
